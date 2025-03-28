049d22902f5a185894df6522a43045efd4150c94
Beibit-gh <6752516@gmail.com>
2025-03-28 21:04:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
-from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters, Update
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from transformers import MarianTokenizer, MarianMTModel
 import torch
 
+# Загружаем модель и токенизатор (казахский -> русский)
 model_name = 'Helsinki-NLP/opus-mt-kk-ru'
 tokenizer = MarianTokenizer.from_pretrained(model_name)
 model = MarianMTModel.from_pretrained(model_name)
@@ -18,9 +20,15 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     translated_text = translate(original_text)
     await update.message.reply_text(translated_text)
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Привет! Отправь текст для перевода с казахского на русский.")
+
 if __name__ == '__main__':
     TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    app = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+    app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), handle_message))
+
     print("Бот запущен...")
     app.run_polling()
